9c26476ed1ce224074b7a105b39d5805e14c181e
JayVae <919433524@qq.com@server.com>
2018-02-01 10:10:50 +0800

    spring mvc权限管理系统。 spring mvc 配置，踩了不少坑。

---
+++ src/main/java/com.mmall.controller/TestController.java
@@ -0,0 +1,25 @@
+package com.mmall.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @Author: Jay
+ * @Date: Created in 22:43 2018/1/30
+ * @Modified By:
+ */
+@Controller
+@RequestMapping("/test")
+@Slf4j
+public class TestController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        log.info("hello");
+        return "hello permission";
+    }
+
+}
---
+++ src/main/java/com.mmall.dao/TestDao.java
@@ -0,0 +1,9 @@
+package com.mmall.dao;
+
+/**
+ * @Author: Jay
+ * @Date: Created in 22:57 2018/1/30
+ * @Modified By:
+ */
+public interface TestDao {
+}
